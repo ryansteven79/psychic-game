da327ee2b733e9ab41ac01237cdc6e61541189e2
ryansteven79 <ryansteven.mueller@gmail.com>
2018-06-02 23:40:53 -0600

    final game

--- assets/psychic-javascript.js
+++ assets/psychic-javascript.js
@@ -6,7 +6,7 @@ var guessesSoFar = [];
 var computerChoice = alphabet[Math.floor(Math.random() * (alphabet.length))].toLowerCase();
 
 function resetGame() {
-    console.log("Game Over!");
+    // console.log("Game Over!");
     guessesLeft = 10;
     wins = 0;
     losses = 0;
@@ -15,52 +15,63 @@ function resetGame() {
     document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
     document.getElementById("guessesSoFar").innerHTML = "Your Guesses So Far: ";
     guessesSoFar = [];
-    console.log(guessesLeft, wins, losses)
+    // console.log(guessesLeft, wins, losses)
     computerChoice = alphabet[Math.floor(Math.random() * (alphabet.length))].toLowerCase();
+    document.getElementById("you-win").style.display = "none";
+    document.getElementById("board").style.display = "block";
+    document.getElementById("btn").style.display = "none";
+
+
 }
 
 function gameOver() {
     document.getElementById("win").innerHTML = "Game Over";
     document.getElementById("loss").innerHTML = "Game Over";
+    resetGame();
     alert("Computer's Choice: " + computerChoice);
+}
+
+function youWin() {
+    var x = document.getElementById("board");
+    x.style.display === "none";
 }
 
 
 
 document.onkeypress = function (event) {
-    var letter = event.key.toLowerCase();
-    guessesSoFar.push(letter);
-    console.log(letter)
-    var x = "";
-    for (var i = 0; i < guessesSoFar.length; i++) {
-        x = x + " " + guessesSoFar[i];
-    }
-    document.getElementById("guessesSoFar").innerHTML = "Your Guesses so far: " + x;
-    if (letter === computerChoice) {
-        console.log("you guessed correctly!");
-        wins++;
+    console.log("keyCode: " + event.keyCode); //helper//
+    console.log(computerChoice);
+    if (event.keyCode < 97 || event.keyCode > 122) {
+        alert("invalid key!")
+        guessesLeft++;
+        losses--;
     } else {
-        console.log("Wrong!!!");
-        losses++;
+        var letter = event.key.toLowerCase();
+        guessesSoFar.push(letter);
+        console.log(letter)
+        guessesSoFar;
+        // var x = "";
+        // for (var i = 0; i < guessesSoFar.length; i++) {
+        //     x = x + " " + guessesSoFar[i];
+        // }
     }
-    document.getElementById("win").innerHTML = "Wins: " + wins;
-    document.getElementById("loss").innerHTML = "Losses: " + losses;
+    // document.getElementById("guessesSoFar").innerHTML = "Your Guesses So Far: " + x;
+    document.getElementById("guessesSoFar").innerHTML = "Your Guesses So Far: " + guessesSoFar;
+    if (letter !== computerChoice) {
+        // console.log("Wrong!!!");
+        losses++;
+        document.getElementById("loss").innerHTML = "Losses: " + losses;
+    } else {
+        wins++;
+        document.getElementById("win").innerHTML = "<h1>You Win!</h1>";
+        document.getElementById("board").style.display = "none";
+        document.getElementById("you-win").style.display = "block";
+        document.getElementById("btn").style.display = "block";
+    }
+    // document.getElementById("win").innerHTML = "Wins: " + wins;
     guessesLeft--;
     document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
     if (guessesLeft <= 0) {
         return gameOver();
     }
 }
-
-
-
-
-
-
-// If user guesses correct letter: wins++, reset game
-
-// if not: loss++, guessesLeft--
-
-// reset the game after guessesLeft === 0.
-
-//
